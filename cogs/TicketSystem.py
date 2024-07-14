b07eacd0c76c7e57f6234a04b996f91a18d8e88a
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-14 13:04:53 +0200

    change: TicketSystem.py
    added TicketView class

--- cogs/TicketSystem.py
+++ cogs/TicketSystem.py
@@ -1,8 +1,14 @@
 from discord.commands import slash_command
 import ezcord
+import discord
+
 
 class TicketSystem(ezcord.Cog, hidden=True):
     pass
 
+
 def setup(bot):
     bot.add_cog(TicketSystem(bot))
+
+
+class TicketView(discord.ui.View):
