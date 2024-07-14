9716f4415c013cf8cc927c91d36736f6e4a0da73
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-14 17:28:12 +0200

    Update update_stats.py
    members - bots

--- update_stats.py
+++ update_stats.py
@@ -1,26 +1,30 @@
 import discord
 
+
 class Updater():
     def __init__(self, bot):
         self.bot = bot
-    
-    
+
     async def update(self):
         channel = await self.bot.fetch_channel(1259432546786349126)
-        
-        try: 
+
+        try:
             message = await channel.fetch_message(1259435457054380103)
-            
+            bots = 0
+            async for member in channel.guild.fetch_members(limit=None):
+                if member.bot:
+                    bots += 1
+
             embed = discord.Embed(
                 title=':chart_with_upwards_trend: Server Stats',
                 color=discord.Color.green()
             )
-            embed.add_field(name='Users', value=f"{channel.guild.member_count}", inline=False)
+            embed.add_field(name='Members', value=f"{channel.guild.member_count - bots}", inline=False)
             embed.add_field(name='Text channels', value=len(channel.guild.text_channels), inline=False)
             embed.add_field(name='Voice channels', value=len(channel.guild.voice_channels), inline=False)
             embed.add_field(name='Roles', value=len(channel.guild.roles), inline=False)
             embed.set_footer(icon_url=self.bot.user.avatar.url, text='Coding Soul - Stats')
-            
+
             await message.edit(embed=embed)
         except discord.NotFound:
             print('Nachricht wurde nicht gefunden')
