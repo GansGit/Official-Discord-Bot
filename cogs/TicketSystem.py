288094cf90e8f4de9acc78569dd4662fbff55135
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-14 13:09:51 +0200

    change: TicketSystem
    added embed

--- cogs/TicketSystem.py
+++ cogs/TicketSystem.py
@@ -2,7 +2,6 @@ from discord.commands import slash_command
 import ezcord
 import discord
 
-
 class TicketSystem(ezcord.Cog, hidden=True):
     pass
 
@@ -14,3 +13,13 @@ def setup(bot):
 class TicketView(discord.ui.View):
     @discord.ui.button(label='Support', style=discord.ButtonStyle.primary, emoji='👮')
     async def button_callback(self, button, interaction: discord.Interaction):
+        guild = interaction.guild
+        channel: discord.TextChannel = await guild.create_text_channel(name=f'support-{interaction.user.id}')
+
+        embed = discord.Embed(
+            title='New Ticket created',
+            description=f'Hey {interaction.user.mention}! Your Support-Ticket was successfully created. (Enter your Question / etc..)',
+            color=discord.Color.brand_green()
+        )
+
+        await channel.send(embed=embed)
