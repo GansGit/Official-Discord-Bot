5580523cb590f31dde9ac0e4168edd5bb306b65f
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-14 13:12:50 +0200

    Update TicketSystem.py
    timeout=None,
    reformat code

--- cogs/TicketSystem.py
+++ cogs/TicketSystem.py
@@ -2,6 +2,7 @@ from discord.commands import slash_command
 import ezcord
 import discord
 
+
 class TicketSystem(ezcord.Cog, hidden=True):
     pass
 
@@ -11,14 +12,18 @@ def setup(bot):
 
 
 class TicketView(discord.ui.View):
-    @discord.ui.button(label='Support', style=discord.ButtonStyle.primary, emoji='👮')
+    def __int__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label='Support', style=discord.ButtonStyle.primary, emoji='👮', custom_id='ticket-support-btn')
     async def button_callback(self, button, interaction: discord.Interaction):
         guild = interaction.guild
         channel: discord.TextChannel = await guild.create_text_channel(name=f'support-{interaction.user.id}')
 
         embed = discord.Embed(
             title='New Ticket created',
-            description=f'Hey {interaction.user.mention}! Your Support-Ticket was successfully created. (Enter your Question / etc..)',
+            description=f'Hey {interaction.user.mention}! Your Support-Ticket was successfully created. (Enter your '
+                        f'Question / etc..)',
             color=discord.Color.brand_green()
         )
 
