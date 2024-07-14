aafc5d247d0ec562699ed553f6b7464785ad4235
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-14 14:36:53 +0200

    Update TicketSystem.py
    added report_bug_button_callback

--- cogs/TicketSystem.py
+++ cogs/TicketSystem.py
@@ -64,3 +64,30 @@ class TicketView(discord.ui.View):
 
         await channel.send(f"{interaction.user.mention}", embed=embed)
         await interaction.response.send_message(f'Ticket channel {channel_name} created.', ephemeral=True)
+
+    async def report_bug_button_callback(self, interaction: discord.Interaction):
+        guild = interaction.guild
+        channel_name = f'support-{interaction.user.id}'
+
+        existing_channel = discord.utils.get(guild.text_channels, name=channel_name)
+        if existing_channel:
+            await interaction.response.send_message(f'A channel with the name {channel_name} already exists.',
+                                                    ephemeral=True)
+            return
+
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            interaction.user: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+        }
+        category = discord.utils.get(guild.categories, id=1256616177887875153)
+        channel: discord.TextChannel = await guild.create_text_channel(name=channel_name, overwrites=overwrites,
+                                                                       category=category)
+
+        embed = discord.Embed(
+            title='New Ticket created',
+            description=f'Your Support-Ticket was successfully created. (Enter your Question / etc..)',
+            color=discord.Color.brand_green()
+        )
+
+        await channel.send(f"{interaction.user.mention}", embed=embed)
+        await interaction.response.send_message(f'Ticket channel {channel_name} created.', ephemeral=True)
