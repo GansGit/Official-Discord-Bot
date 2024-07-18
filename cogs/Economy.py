96fef09cfce1ea4ae8d2db5c934b9174acb1184e
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-18 17:08:21 +0200

    Update Economy.py

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -8,11 +8,18 @@ from EconomyManager import Manager
 
 
 class Economy(ezcord.Cog, emoji='🍪', description="Show information about the bot", hidden=True):
+    def __int__(self, bot):
+        self.bot = bot
+        self.manager = Manager(self=bot) # defining database manager
     @commands.Cog.listener()
     async def on_ready(self):
-        Manager.create_database()
-        
+        # creating economy database, if not exists
+        self.manager.create_database()
 
+    @slash_command()
+    async def setup(self, ctx:discord.ApplicationContext):
+        self.manager.createUser(user_id=ctx.user.id)
+        await ctx.respond('Created user', ephemeral=True)
 
 def setup(bot):
     bot.add_cog(Economy(bot))
