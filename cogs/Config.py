609f088dc0bdb126eb298b4bff6ffd8d0056e2ca
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-21 22:11:52 +0200

    Update Config.py

--- cogs/Config.py
+++ cogs/Config.py
@@ -2,9 +2,6 @@ import json
 
 class Config():
     
-    def __init__():
-        pass
-    
     def get_config(name):
         with open("config.json", "r") as f:
             json_file = json.load(f)
