676fd977ba8c5395fe0a5ed52fd9363607d5d1eb
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-25 11:50:51 +0200

    Update EconomyManager.py

--- EconomyManager.py
+++ EconomyManager.py
@@ -1,35 +1,36 @@
 import sqlite3
 
 
-class Manager:
-    def __int__(self, bot):
-        self.bot = bot
+def doesUserExist(self, user_id):
+    pass
 
-    def doesUserExist(self, user_id):
-        pass
 
-    def createUser(self, user_id):
-        conn: sqlite3.Connection = sqlite3.connect('economy.db')
-        cursor = conn.cursor()
-        qry = "INSERT INTO users (id, bank, wallet) VALUES (?,?,?)"
-        cursor.execute(qry, (user_id, 100, 0))
+def createUser(self, user_id):
+    conn: sqlite3.Connection = sqlite3.connect('economy.db')
+    cursor = conn.cursor()
+    qry = "INSERT INTO users (id, bank, wallet) VALUES (?,?,?)"
+    cursor.execute(qry, (user_id, 100, 0))
 
-    def addCoins(self, user_id, amount):
-        conn: sqlite3.Connection = sqlite3.connect('economy.db')
-        cursor = conn.cursor()
 
-    def removeCoins(self, user_id, amount):
-        pass
+def addCoins(self, user_id, amount):
+    conn: sqlite3.Connection = sqlite3.connect('economy.db')
+    cursor = conn.cursor()
 
-    def getCoins(self, user_id):
-        pass
 
-    def create_database(self):
-        conn: sqlite3.Connection = sqlite3.connect('economy.db')
-        cursor = conn.cursor()
-        qry = """"
+def removeCoins(self, user_id, amount):
+    pass
+
+
+def getCoins(self, user_id):
+    pass
+
+
+def create_database(self):
+    conn: sqlite3.Connection = sqlite3.connect('economy.db')
+    cursor = conn.cursor()
+    qry = """"
                 CREATE TABLE IF NOT EXISTS users
                 (id INTEGER PRIMARY KEY, bank INTEGER, wallet INTEGER)
                 """
-        cursor.execute(qry)
-        conn.commit()
+    cursor.execute(qry)
+    conn.commit()
