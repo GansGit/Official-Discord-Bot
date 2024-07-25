47ada98701cb07c17064c0932d26f1929525c22a
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-25 13:13:48 +0200

    Update LevelSystem.py
    added rank command for other users

--- cogs/LevelSystem.py
+++ cogs/LevelSystem.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from discord.commands import slash_command
+from discord.commands import slash_command, option
 import discord
 import ezcord
 
@@ -10,15 +10,16 @@ from easy_pil import Editor, load_image_async, Font
 
 from cogs.Config import Config
 
+
 class LevelSystem(ezcord.Cog, emoji='✨'):
     @commands.Cog.listener()
-    async def on_message(self, message : discord.Message):
+    async def on_message(self, message: discord.Message):
         if message.author.bot:
             return
-        
+
         def xp_to_level(xp, k=100):
-            result = math.sqrt(xp/k)
-            
+            result = math.sqrt(xp / k)
+
             if result <= 0:
                 return 1
             else:
@@ -26,86 +27,83 @@ class LevelSystem(ezcord.Cog, emoji='✨'):
                     return math.floor(result)
                 else:
                     return 1
-        
+
         def load_json():
             with open('levels.json', 'r') as file:
                 return json.load(file)
-    
+
         def save_json(data):
             with open('levels.json', 'w') as file:
                 json.dump(data, file, indent=4)
-            
+
         def add_or_update_xp(data, user_id, xp_to_add):
             user_id_str = str(user_id)
             if user_id_str in data:
                 data[user_id_str]['xp'] += xp_to_add
                 data[user_id_str]['level'] = xp_to_level(data[user_id_str]['xp'])
-                
+
             else:
-                data[user_id_str] = {'xp':xp_to_add, 'level':1}
+                data[user_id_str] = {'xp': xp_to_add, 'level': 1}
             return data
-            
+
         channels = Config.get_config('level-system')['allowed-channels']
-        
-        
+
         for channel in channels:
             if message.channel.id == channel:
                 random_xp = random.randint(5, 20)
                 data = load_json()
                 user_id = str(message.author.id)
-                
-                
-                print('Received message: ' + message.content + ", xp: " + str(random_xp)) # Logging for control
-                
+
+                print('Received message: ' + message.content + ", xp: " + str(random_xp))  # Logging for control
+
                 data = add_or_update_xp(data=data, user_id=user_id, xp_to_add=random_xp)
-                
+
                 save_json(data=data)
                 current_level = data[user_id]['level']
-                
+
                 new_data = load_json()
                 if new_data[user_id]['level'] > current_level:
                     new_level = new_data[user_id]['level']
                     await message.channel.send(f'{message.author.mention} achieved a new level: {new_level}')
-    
+
     @slash_command(name='rank', description="Show's the current rank.")
-    async def rank(self, ctx):
+    @option("user", description="Pick a user")
+    async def rank(self, ctx: discord.ApplicationContext, user: discord.User = None):
+        if user is None:
+            user = ctx.author
+
         def get_user_level(user_id):
             with open('levels.json', 'r') as file:
-                json_file =  json.load(file)
+                json_file = json.load(file)
             return json_file[str(user_id)]['level']
-        
+
         def get_user_xp(user_id):
             with open('levels.json', 'r') as file:
                 json_file = json.load(file)
             return json_file[str(user_id)]['xp']
-            
-        
+
         await ctx.defer()
         background = Editor("img/space.png").resize((800, 250))
 
-        avatar = await load_image_async(ctx.author.display_avatar.url)
+        avatar = await load_image_async(user.display_avatar.url)
         circle_avatar = Editor(avatar).resize((200, 200)).circle_image()
-        
-        background.paste(circle_avatar, (25,25))
-        
-        current_xp = get_user_xp(ctx.author.id)
-        current_level = get_user_level(ctx.author.id)
-        
+
+        background.paste(circle_avatar, (25, 25))
+
+        current_xp = get_user_xp(user.id)
+        current_level = get_user_level(user.id)
+
         big_text = Font.poppins(size=50, variant="bold")
-        background.text((490, 50), f'{ctx.author.display_name}', color="white", font=big_text, align="center")
+        background.text((490, 50), f'{user.display_name}', color="white", font=big_text, align="center")
         small_text = Font.poppins(size=30, variant='regular')
         background.text(
             (490, 125), f'Level: {current_level} XP: {current_xp}', color="#00ced1", font=small_text, align="center"
         )
-        
-        
+
         file = discord.File(fp=background.image_bytes, filename='rank.png')
-        
-        
+
         await ctx.followup.send(file=file)
-        
-    
-    
-        
+
+
 def setup(bot):
     bot.add_cog(LevelSystem(bot))
