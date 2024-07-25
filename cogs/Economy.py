f0f4f3255b49e0fb612906be00821383271fe826
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-25 12:08:51 +0200

    Update Economy.py

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -1,17 +1,25 @@
 import ezcord
 import discord
+import logging
 
+import EconomyManager
 from discord.ext import commands
-
 
 class Economy(ezcord.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # On-Cog-Load
     @commands.Cog.listener()
     async def on_ready(self):
-        pass
+        # Creating database if not exists
+        try:
+            EconomyManager.create_database()
+            print('Database is now available!')
+        except Exception as e:
+            print(f'Error creating database: {e}')
 
 
+# setup for the cog
 def setup(bot):
     bot.add_cog(Economy(bot))
