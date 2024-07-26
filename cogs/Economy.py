70cf89364903646159cecf974d10447ba64a94e5
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-26 18:29:31 +0200

    feat: added casino, withdraw, deposit and more

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 import random
 
@@ -9,6 +10,29 @@ from utils import EconomyManager
 from discord.ext import commands
 from discord.commands import option, slash_command
 from cogs.Config import Config
+
+
+async def random_slot():
+    emoji_list = ['🍎', '🪙', '🍐', '🍆']
+    rs: list = []
+
+    while len(rs) < 3:
+        rs.append(random.choice(emoji_list))
+    return rs
+
+
+async def check_prize(rs_list: list) -> int:
+    if len(rs_list) != 3:
+        raise ValueError()
+
+    count = 0
+
+    if rs_list[0] == rs_list[1]:
+        count += 1
+    if rs_list[1] == rs_list[2]:
+        count += 1
+
+    return count
 
 
 class Economy(ezcord.Cog, emoji='💵'):
@@ -57,7 +81,7 @@ class Economy(ezcord.Cog, emoji='💵'):
 
         earnings = random.randint(25, 250)
 
-        EconomyManager.addCoins(ctx.user.id, earnings, 'bank')  # adding coins in database
+        EconomyManager.add_coins(ctx.user.id, earnings, 'bank')  # adding coins in database
         await ctx.respond(
             f"You've worked as a freelancer and earned {earnings} <:souls:1266390733821317230> ")  # responding to user
 
@@ -66,7 +90,7 @@ class Economy(ezcord.Cog, emoji='💵'):
     @option('User', description='Pick a user where you want to', required=True)
     @option('Method', description='Pick the method', choices=['bank', 'wallet'], required=True)
     async def add_coins(self, ctx, user: discord.User, method: str, amount: int):
-        EconomyManager.addCoins(user.id, amount, method=method)
+        EconomyManager.add_coins(user.id, amount, method=method)
         await ctx.respond(f'Added {amount} <:souls:1266390733821317230> to {user.mention}', ephemeral=True)
 
     @slash_command(name='leaderboard', description='Check the leaderboard!')
@@ -116,6 +140,85 @@ class Economy(ezcord.Cog, emoji='💵'):
         )
         embed.set_footer(text='Coding Soul - Economy', icon_url=Config.get_config('footer')['icon-url'])  # set footer
         await ctx.respond(embed=embed)  # responding user
+
+    @slash_command(name='slots', description='Play some slots, bro!')
+    @option(name='Amount', required=True, description='Select the amount you wanna play with!')
+    async def slots(self, ctx: discord.ApplicationContext, amount: int):
+
+        current_coins = EconomyManager.get_coins(ctx.user.id, method='wallet')
+
+        if current_coins < amount:
+            await ctx.respond('To less money to play!', ephemeral=True)
+            return
+        if amount < 0:
+            await ctx.respond("You can't play with 0 or negative bet!", ephemeral=True)
+            return
+
+        # First slot row
+        slot: list = await random_slot()
+        embed = discord.Embed(
+            title='Slots',
+            description=f'{slot[0]} | {slot[1]} | {slot[2]}',
+            color=discord.Color.orange()
+        )
+        msg: discord.Message = await ctx.respond(embed=embed)
+        await asyncio.sleep(1)
+
+        a = 0
+        while a < 3:
+            # Going through 3x random slots
+            slot: list = await random_slot()
+            embed = discord.Embed(
+                title='Slots',
+                description=f'{slot[0]} | {slot[1]} | {slot[2]}',
+                color=discord.Color.orange()
+            )
+            await msg.edit(embed=embed)
+            await asyncio.sleep(1)
+            a += 1
+
+        rs = await check_prize(slot)
+        if rs == 0:
+            await ctx.respond(f"You've lost {amount} <:souls:1266390733821317230>")
+            EconomyManager.remove_coins(user_id=ctx.user.id, method='wallet', amount=amount)
+        elif rs == 1:
+            await ctx.respond(f"You've got {amount * 2} <:souls:1266390733821317230>")
+            EconomyManager.add_coins(user_id=ctx.user.id, method='wallet', amount=amount * 2)
+        elif rs == 2:
+            await ctx.respond(f"You've got {amount * 3} <:souls:1266390733821317230>")
+            EconomyManager.add_coins(user_id=ctx.user.id, method='wallet', amount=amount * 3)
+
+    @slash_command(name='withdraw', description='Withdraw some money!')
+    @option(name='amount', description='The amount of the money.')
+    async def withdraw(self, ctx: discord.ApplicationContext, amount: int):
+        current_coins = EconomyManager.get_coins(ctx.user.id, method='bank')
+
+        if current_coins < amount:
+            await ctx.respond("You have too less money on the bank!", ephemeral=True)
+            return
+        if amount < 0:
+            await ctx.respond("You can't withdraw 0 or negative money!", ephemeral=True)
+            return
+
+        EconomyManager.remove_coins(user_id=ctx.user.id, method='bank', amount=amount)
+        EconomyManager.add_coins(user_id=ctx.user.id, method='wallet', amount=amount)
+        await ctx.respond("The money was successfully withdrawn.", ephemeral=True)
+
+    @slash_command(name='deposit', description='Deposit some money!')
+    @option(name='amount', description='The amount of the money.')
+    async def deposit(self, ctx: discord.ApplicationContext, amount: int):
+        current_coins = EconomyManager.get_coins(ctx.user.id, method='wallet')
+
+        if current_coins < amount:
+            await ctx.respond("You have too less money on the wallet!", ephemeral=True)
+            return
+        if amount < 0:
+            await ctx.respond("You deposit 0 or negative money!", ephemeral=True)
+            return
+
+        EconomyManager.remove_coins(user_id=ctx.user.id, method='wallet', amount=amount)
+        EconomyManager.add_coins(user_id=ctx.user.id, method='bank', amount=amount)
+        await ctx.respond("The money was successfully deposited.", ephemeral=True)
 
 
 # setup for the cog
